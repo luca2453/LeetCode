e425e1239c271a35c6923af20b823fc9f2c0d680
Luca <58530139+luca2453@users.noreply.github.com>
2023-10-30 15:29:21 -0300

    Delete Eazy directory

--- Eazy/Python3/1512-number-of-good-pairs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def numIdenticalPairs(self, nums: List[int]) -> int:
-        count = 0
-        tamanho = len(nums)
-        for i in range(tamanho):
-            for j in range(tamanho):
-                if i < j and nums[i] == nums[j]:
-                    count += 1
-        return count
